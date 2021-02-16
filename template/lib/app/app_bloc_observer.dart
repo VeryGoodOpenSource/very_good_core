7a15c9b7b744e04b31106ddaa5b93f36379e843b
Felix Angelov <felangelov@gmail.com>
2021-02-15 21:00:15 -0600

    feat: created very_good_core 🦄

---
+++ template/lib/app/app_bloc_observer.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log('onTransition(${bloc.runtimeType}, $transition)');
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
