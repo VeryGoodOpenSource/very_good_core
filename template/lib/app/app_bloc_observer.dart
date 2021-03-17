b1b257d070d3661e56928cb3f765169274c2f752
Felix Angelov <felangelov@gmail.com>
2021-03-16 21:34:14 -0500

    chore/upgrade-bloc

--- template/lib/app/app_bloc_observer.dart
+++ template/lib/app/app_bloc_observer.dart
@@ -4,13 +4,13 @@ import 'package:bloc/bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    log('onTransition(${bloc.runtimeType}, $transition)');
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)');
   }
 
   @override
-  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     log('onError(${bloc.runtimeType}, $error, $stackTrace)');
     super.onError(bloc, error, stackTrace);
   }
