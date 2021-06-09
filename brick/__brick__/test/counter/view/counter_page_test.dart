83cc3923e4f3200edec307b75df84ae7933b3207
VGV <vgvbot@users.noreply.github.com>
2021-06-09 15:51:18 +0000

    chore: generate very_good_core template

--- brick/__brick__/test/counter/view/counter_page_test.dart
+++ brick/__brick__/test/counter/view/counter_page_test.dart
@@ -5,11 +5,12 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+
 import 'package:{{project_name}}/counter/counter.dart';
 
 import '../../helpers/helpers.dart';
