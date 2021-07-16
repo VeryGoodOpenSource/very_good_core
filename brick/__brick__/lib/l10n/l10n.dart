a9ebe9aa3aea9ca39d54b49f28cfde418fe1bab1
VGV <vgvbot@users.noreply.github.com>
2021-07-16 02:32:08 +0000

    chore: generate very_good_core template

--- brick/__brick__/lib/l10n/l10n.dart
+++ brick/__brick__/lib/l10n/l10n.dart
@@ -11,5 +11,5 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
+  AppLocalizations get l10n => AppLocalizations.of(this);
 }
