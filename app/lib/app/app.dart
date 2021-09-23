9aae6e1f50868a5307669601e441f14ac8d13646
Felix Angelov <felangelov@gmail.com>
2021-09-23 16:05:40 -0500

    refactor: bootstrap

--- app/lib/app/app.dart
+++ app/lib/app/app.dart
@@ -1,2 +1,1 @@
-export 'app_bloc_observer.dart';
 export 'view/app.dart';
--- app/lib/main_production.dart
+++ app/lib/main_production.dart
@@ -1,18 +1,6 @@
-import 'dart:async';
-import 'dart:developer';
-
-import 'package:bloc/bloc.dart';
-import 'package:flutter/widgets.dart';
 import 'package:very_good_core/app/app.dart';
+import 'package:very_good_core/bootstrap.dart';
 
 void main() {
-  Bloc.observer = AppBlocObserver();
-  FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
-
-  runZonedGuarded(
-    () => runApp(const App()),
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
-  );
+  bootstrap(() => const App());
 }
