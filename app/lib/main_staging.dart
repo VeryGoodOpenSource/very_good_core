7383db824f2721e380f4aa9a1740efce4dfd7bc3
Felix Angelov <felangelov@gmail.com>
2021-09-23 15:21:56 -0500

    refactor: app organization and simplification

--- app/lib/main_staging.dart
+++ app/lib/main_staging.dart
@@ -3,9 +3,7 @@ import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
-
 import 'package:very_good_core/app/app.dart';
-import 'package:very_good_core/app/app_bloc_observer.dart';
 
 void main() {
   Bloc.observer = AppBlocObserver();
