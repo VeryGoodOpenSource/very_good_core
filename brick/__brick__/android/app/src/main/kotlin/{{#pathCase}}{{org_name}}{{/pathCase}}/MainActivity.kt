8a7510c7a11ceac1364d9df7fe25d7fd51c9b33e
Marcos Sevilla <marcossevilla14@gmail.com>
2021-09-30 12:14:36 -0600

    remove extra folder

--- brick/__brick__/android/app/src/main/kotlin/{{#pathCase}}{{org_name}}{{/pathCase}}/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package {{#org_name}}{{#snakeCase}}{{value}}{{/snakeCase}}{{separator}}{{/org_name}}
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
