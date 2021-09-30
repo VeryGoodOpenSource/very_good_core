552e043a7415a494a8a4d9bebf1d754dbb47811a
Marcos Sevilla <marcossevilla14@gmail.com>
2021-09-30 12:10:32 -0600

    revert files on brick to main

---
+++ brick/__brick__/android/app/src/main/kotlin/{{#org_name}}{{#snakeCase}}{{value}}{{/snakeCase}}/{{/org_name}}/MainActivity.kt
@@ -0,0 +1,6 @@
+package {{#org_name}}{{#snakeCase}}{{value}}{{/snakeCase}}{{separator}}{{/org_name}}
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
