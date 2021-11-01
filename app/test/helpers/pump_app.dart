6f9f68fbc0de074900446f49e8dbc6e66c742d24
Felix Angelov <felangelov@gmail.com>
2021-11-01 14:03:34 -0500

    chore(deps): upgrade dependencies

--- app/test/helpers/pump_app.dart
+++ app/test/helpers/pump_app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:very_good_core/l10n/l10n.dart';
