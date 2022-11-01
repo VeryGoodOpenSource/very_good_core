fd9ab566481be666b4ccb761b942e648328a5858
Erick Zanardo <erickzanardoo@gmail.com>
2022-11-01 19:14:08 -0300

    pr suggestions

--- brick/hooks/pre_gen.dart
+++ brick/hooks/pre_gen.dart
@@ -1,21 +1,27 @@
 import 'package:mason/mason.dart';
 import 'package:recase/recase.dart';
 
+enum Platform {
+  android,
+  ios,
+}
+
 void run(HookContext context) {
   context.vars['current_year'] = DateTime.now().year;
 
-  context.vars['application_id_android'] = _appId(context, isAndroid: true);
-  context.vars['application_id_ios'] = _appId(context, isAndroid: false);
+  context.vars['application_id_android'] =
+      _appId(context, platform: Platform.android);
+  context.vars['application_id_ios'] = _appId(context, platform: Platform.ios);
 }
 
-String _appId(HookContext context, { required bool isAndroid }) {
+String _appId(HookContext context, {required Platform platform}) {
   final orgName = context.vars['org_name'];
   final projectName = context.vars['project_name'];
 
   String? applicationId = context.vars['application_id'];
   applicationId = (applicationId?.isNotEmpty ?? false)
       ? applicationId
-      : '${orgName}.${isAndroid ? ReCase(projectName).snakeCase : ReCase(projectName).paramCase}';
+      : '${orgName}.${platform == Platform.android ? ReCase(projectName).snakeCase : ReCase(projectName).paramCase}';
 
   return applicationId!;
 }
