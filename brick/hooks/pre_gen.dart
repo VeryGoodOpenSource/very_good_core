a8a7ec6e23d624d6a62944fecc3f2c409f57254b
Erick <erickzanardoo@gmail.com>
2022-11-10 15:08:13 -0300

    feat: adding application id parameter (#190)
    * feat: adding application id parameter
    
    * feat: PR suggestions
    
    * pr suggestions
    
    * PR suggestions
    
    * feat: updating missing files
    
    * pr suggestion

--- brick/hooks/pre_gen.dart
+++ brick/hooks/pre_gen.dart
@@ -1,5 +1,27 @@
 import 'package:mason/mason.dart';
+import 'package:recase/recase.dart';
+
+enum Platform {
+  android,
+  ios,
+}
 
 void run(HookContext context) {
   context.vars['current_year'] = DateTime.now().year;
+
+  context.vars['application_id_android'] =
+      _appId(context, platform: Platform.android);
+  context.vars['application_id'] = _appId(context);
+}
+
+String _appId(HookContext context, {Platform? platform}) {
+  final orgName = context.vars['org_name'];
+  final projectName = context.vars['project_name'];
+
+  String? applicationId = context.vars['application_id'];
+  applicationId = (applicationId?.isNotEmpty ?? false)
+      ? applicationId
+      : '${orgName}.${platform == Platform.android ? ReCase(projectName).snakeCase : ReCase(projectName).paramCase}';
+
+  return applicationId!;
 }
