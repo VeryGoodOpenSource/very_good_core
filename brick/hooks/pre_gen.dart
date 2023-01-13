78e6052e484373168f2f6d77e6f587bd6cc7758c
Felix Angelov <felangelov@gmail.com>
2023-01-13 14:13:00 -0600

    add very_good_analysis

--- brick/hooks/pre_gen.dart
+++ brick/hooks/pre_gen.dart
@@ -15,10 +15,10 @@ String _appId(HookContext context, {Platform? platform}) {
   final orgName = context.vars['org_name'] as String;
   final projectName = context.vars['project_name'] as String;
 
-  String? applicationId = context.vars['application_id'];
+  var applicationId = context.vars['application_id'] as String?;
   applicationId = (applicationId?.isNotEmpty ?? false)
       ? applicationId
-      : '${orgName}.${platform == Platform.android ? projectName.snakeCase : projectName.paramCase}';
+      : '''$orgName.${platform == Platform.android ? projectName.snakeCase : projectName.paramCase}''';
 
   return applicationId!;
 }
