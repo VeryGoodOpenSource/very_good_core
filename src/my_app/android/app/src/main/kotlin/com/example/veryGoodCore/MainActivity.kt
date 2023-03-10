d9546857d9120565481fece1c5171928e63513ea
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-03-10 12:29:54 +0000

    infra changes

--- src/my_app/android/app/src/main/kotlin/com/example/veryGoodCore/MainActivity.kt
+++ src/my_app/android/app/src/main/kotlin/com/example/veryGoodCore/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.veryGoodCore
 
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
 }
