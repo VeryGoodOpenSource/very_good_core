239ec8e23b4146dcd907e81c299f21ba79694582
Alejandro Santiago <dev@alestiago.com>
2023-11-02 11:55:19 +0000

    chore: copy brick into very_good_core2

---
+++ very_good_core2/__brick__/{{project_name.snakeCase()}}/lib/app/view/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:{{project_name.snakeCase()}}/counter/counter.dart';
+import 'package:{{project_name.snakeCase()}}/l10n/l10n.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const CounterPage(),
+    );
+  }
+}
---
+++ very_good_core2/__brick__/{{project_name.snakeCase()}}/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:{{project_name.snakeCase()}}/app/app.dart';
+import 'package:{{project_name.snakeCase()}}/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
